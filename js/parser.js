219cc11c294d76da915873ee63c444fa3edecefb
ibrohim777775 <ibrohim777775@gmail.com>
2021-07-04 02:07:47 +0300

    edited parser.js

--- js/parser.js
+++ js/parser.js
@@ -1,17 +1,33 @@
-const body = document.querySelector("body");
-body.style.backgroundColor = "#f2f2f2";
 window.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body");
+  let textNodes = [];
+
   function recurcy(element) {
     element.childNodes.forEach((node) => {
       // console.log(node);
-      if (node.nodeName === "#text") {
-        return;
+      if (node.nodeName.match(/^H\d/)) {
+        const obj = {
+          header: node.nodeName,
+          content: node.textContent.trim(),
+        };
+        // console.log(node);
+        textNodes.push(obj);
       } else {
-        console.log(node);
         recurcy(node);
       }
     });
   }
   recurcy(body);
+  // console.log(textNodes);
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(textNodes),
+  })
+    .then((res) => res.json())
+    .then((response) => console.log(response));
 });
-console.log("salomm");
+// console.log("salomm");
